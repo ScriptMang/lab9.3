ba694db49d1920f39753749ce71245ba2dd003cb
scriptMang <aperalta686@gmail.com>
2026-03-08 23:26:11 -0400

    Fix html for task-item func by making div with id task-container the outer div that wraps all elems

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -5,31 +5,34 @@ import type {TaskItemProps} from './../types'
 function TaskItem({task, onStatusChange, onDelete}: TaskItemProps) {
   
     return (
-        <>
+      <>
         <div id="taskContainer">
+          <div id="taskTitleInfo1">
             <div id="taskTitle">
-                <h3>{task.title}</h3>
+              <h3>{task.title}</h3>
             </div>
             <div id="taskItemOptions">
-                <select defaultValue={ task.status } onChange={() => {
-                     onStatusChange(
-                        "3", "completed"
-                     );
-                }}>
-                    <option value="pending">Pending</option>
-                    <option value="in-progress">In Progress</option>
-                    <option value="completed">Completed</option>
-                </select>
-                <button onClick = {()=> onDelete(task.id)}>Delete</button>
+              <select
+                defaultValue={task.status}
+                onChange={() => {
+                  onStatusChange("3", "completed");
+                }}
+              >
+                <option value="pending">Pending</option>
+                <option value="in-progress">In Progress</option>
+                <option value="completed">Completed</option>
+              </select>
+              <button onClick={() => onDelete(task.id)}>Delete</button>
             </div>
-        </div>
-        <p>{task.description}</p>
-        <div>
+          </div>
+          <p>{task.description}</p>
+          <div>
             <span>Priority: {task.priority}</span>
             <span>Due: {task.dueDate}</span>
+          </div>
         </div>
-        </>
-    )
+      </>
+    );
 }
 
 export default TaskItem
