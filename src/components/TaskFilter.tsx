d2ab2d6cedf6647b8b3d8c937a54be4149c43784
scriptMang <aperalta686@gmail.com>
2026-03-11 11:04:58 -0400

    Add filter labels

--- src/components/TaskFilter.tsx
+++ src/components/TaskFilter.tsx
@@ -10,21 +10,23 @@ import type {TaskStatus} from './../types/index'
 
 function TaskFilter({onFilterChange}:TaskFilterProps ){
     return (
-        <>
-        <select defaultValue="All Statuses" onChange={(e) => onFilterChange({status: e.target.value as TaskStatus})}>
+        <div id="taskFilterContainer">
+        <label htmlFor="statusFilter">Status</label>
+        <select id="statusFilter" defaultValue="All Statuses" onChange={(e) => onFilterChange({status: e.target.value as TaskStatus})}>
             <option value="pending">pending</option>
             <option value="in-progress">in-progress</option>
             <option value="completed">completed</option>
             <option value="All Statuses">All Statues</option>
         </select>
 
-         <select defaultValue="All Priorities" onChange={(e) => onFilterChange({priority: (e.target.value as 'low'|'medium'|'high')})}>
+        <label htmlFor="priorityFilter">Priority</label>
+         <select  id="priorityFilter" defaultValue="All Priorities" onChange={(e) => onFilterChange({priority: (e.target.value as 'low'|'medium'|'high')})}>
             <option value="low">low</option>
             <option value="medium">medium</option>
             <option value="high">high</option>
             <option value="All Priorities">All Priorities</option>
         </select>
-        </>
+        </div>
     )
 }
 
