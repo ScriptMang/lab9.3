66810526e907e1afc474d3caaa88b67778c58ff2
scriptMang <aperalta686@gmail.com>
2026-03-11 02:19:50 -0400

    Add on-change attribs to task-filter func

--- src/components/TaskFilter.tsx
+++ src/components/TaskFilter.tsx
@@ -1,4 +1,5 @@
 import type {TaskFilterProps} from './../types/index.ts'
+import type {TaskStatus} from './../types/index'
 
 // export interface TaskFilterProps {
 //   onFilterChange: (filters: {
@@ -10,14 +11,14 @@ import type {TaskFilterProps} from './../types/index.ts'
 function TaskFilter({onFilterChange}:TaskFilterProps ){
     return (
         <>
-        <select defaultValue="All Statuses">
+        <select defaultValue="All Statuses" onChange={(e) => onFilterChange({status: e.target.value as TaskStatus})}>
             <option value="pending">pending</option>
             <option value="in-progress">in-progress</option>
             <option value="completed">completed</option>
             <option value="All Statuses">All Statues</option>
         </select>
 
-         <select defaultValue="All Priorities">
+         <select defaultValue="All Priorities" onChange={(e) => onFilterChange({priority: (e.target.value as 'low'|'medium'|'high')})}>
             <option value="low">low</option>
             <option value="medium">medium</option>
             <option value="high">high</option>
